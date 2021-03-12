cd5d72363e320bafaebec495aec3706609d3b2df
DesireeBesnard <desiree.besnard@gmail.com>
2021-03-12 19:17:36 +0100

    animated span done

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+// Variables declarations
+
+const btnHamburger = document.querySelector('#btnHamburger');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click burger');
+
+    if (btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    } else {
+        btnHamburger.classList.add('open');
+    }
+});
