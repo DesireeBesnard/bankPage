eb315d42b63c69583656bdbc7e9ff36a332074cd
DesireeBesnard <desiree.besnard@gmail.com>
2021-03-31 16:54:12 +0200

    menu done

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,26 @@
 // Variables declarations
 
 const btnHamburger = document.querySelector('#btnHamburger');
-
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click burger');
 
-    if (btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
+    if (header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+        mobileMenu.classList.remove('fade-in');
+        mobileMenu.classList.add('fade-out');
+        
     } else {
-        btnHamburger.classList.add('open');
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+        mobileMenu.classList.remove('fade-out');
+        mobileMenu.classList.add('fade-in');
+        
     }
 });
